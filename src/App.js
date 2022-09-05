6f6c1e76488f5419f385f6eb648fe826c5bede8c
UtkirMansurovich <utkir_xan@mail.ru>
2022-09-05 15:55:16 +0500

    type first code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from './img/Logo.png'
+
+function App() {
+  return (
+    <div>
+      <img src={Logo} alt="Logo"/>
+    </div>
+  );
+}
+
+export default App;
